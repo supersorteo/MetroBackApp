b162be922cfda90af709925dd784202f113c5498
supersorteo <soporte.supersorteo@gmail.com>
2026-03-27 22:30:46 -0400

    subiendo

--- src/main/java/com/example/bdMetro/payments/config/PaymentsConfig.java
+++ src/main/java/com/example/bdMetro/payments/config/PaymentsConfig.java
@@ -1,17 +1,24 @@
 package com.example.bdMetro.payments.config;
 
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 @EnableConfigurationProperties({MercadoPagoProperties.class, MembershipCatalogProperties.class, FxRateProperties.class, PayPalProperties.class})
 public class PaymentsConfig {
 
+    private HttpComponentsClientHttpRequestFactory apacheFactory() {
+        return new HttpComponentsClientHttpRequestFactory(HttpClients.createDefault());
+    }
+
     @Bean
     RestClient mercadoPagoRestClient(MercadoPagoProperties properties) {
         return RestClient.builder()
+                .requestFactory(apacheFactory())
                 .baseUrl(properties.getBaseUrl())
                 .build();
     }
@@ -19,6 +26,7 @@ public class PaymentsConfig {
     @Bean
     RestClient fxRestClient(FxRateProperties properties) {
         return RestClient.builder()
+                .requestFactory(apacheFactory())
                 .baseUrl(properties.getBaseUrl())
                 .build();
     }
@@ -26,6 +34,7 @@ public class PaymentsConfig {
     @Bean
     RestClient payPalRestClient(PayPalProperties properties) {
         return RestClient.builder()
+                .requestFactory(apacheFactory())
                 .baseUrl(properties.getBaseUrl())
                 .build();
     }
